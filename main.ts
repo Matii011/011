d5809080ac714f509b5dfcc6091183b5582f8980
Matii011 <111192644+Matii011@users.noreply.github.com>
2022-08-13 14:18:17 -0300

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(0, 6))
+input.onButtonPressed(Button.A, function () {
+    tronco_superior_dinosaurio.change(LedSpriteProperty.Y, -1)
+    tronco_inferior_dinosaurio.change(LedSpriteProperty.Y, -1)
+    basic.pause(1000)
+    tronco_superior_dinosaurio.change(LedSpriteProperty.Y, 1)
+    tronco_inferior_dinosaurio.change(LedSpriteProperty.Y, 1)
+})
+function piedrita () {
+    piedra = game.createSprite(4, 4)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(1000)
+        piedra.change(LedSpriteProperty.X, -1)
+        if (piedra.isTouching(tronco_inferior_dinosaurio)) {
+            basic.showIcon(IconNames.Sad)
+            music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+            piedrita()
+        }
+    }
+}
+let piedra: game.LedSprite = null
+let tronco_superior_dinosaurio: game.LedSprite = null
+let tronco_inferior_dinosaurio: game.LedSprite = null
+tronco_inferior_dinosaurio = game.createSprite(0, 4)
+tronco_superior_dinosaurio = game.createSprite(0, 3)
+piedrita()
+basic.forever(function () {
+    if (piedra.isTouchingEdge()) {
+        basic.pause(1000)
+        piedra.delete()
+        piedrita()
+    }
 })
